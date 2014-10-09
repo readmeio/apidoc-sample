50edef85e1674086375d80fa43c069d1476480f5
Gregory Koberger <gkoberger@gmail.com>
2014-10-08 21:16:35 -0700

    Sample app using apidoc

---
+++ test.js
@@ -0,0 +1,54 @@
+/**
+ * @api {get} /user/:id Request User information
+ * @apiGroup GetUser
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
+function user() {
+  return 'hello';
+}
+
+/**
+ * @api {post} /user Create User information
+ * @apiGroup PostUser
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
+function user() {
+  return 'hello';
+}
+
+/**
+ * @api {get} /order/:id Request Order information
+ * @apiName GetOrder
+ * @apiGroup Order
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
+function user() {
+  return 'hello';
+}
+
+/**
+ * @api {post} /order Create a New Order
+ * @apiName PostOrder
+ * @apiGroup Order
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
+function user() {
+  return 'hello';
+}
+
