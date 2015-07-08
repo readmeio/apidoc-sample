a0aa8e5a719c28920e3ae5a045a2d08605db9929
Gregory Koberger <gkoberger@gmail.com>
2015-07-08 00:03:14 -0700

    Testing out required/optional

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
  *
  * @apiParam {Number} id Users unique ID.
  *
- * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} [firstname] Firstname of the User.
  * @apiSuccess {String} lastname  Lastname of the User.
  */
 
